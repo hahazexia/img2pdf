b4d10515b70203a8cbea99ff3a02df2a2b674712
hahazexia <hahazexia@outlook.com>
2024-11-22 08:29:45 +0800

    feat: pdf

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use image::codecs::jpeg::JpegDecoder;
 use image::codecs::png::PngDecoder;
+use image::codecs::webp::WebPDecoder;
+use image::GenericImageView as _;
 use printpdf::{
-    ColorBits, ColorSpace, Image, ImageTransform, ImageXObject, Mm, PdfDocument, Px, PdfConformance, CustomPdfConformance,
+    Image, ImageTransform, Mm, Px, PdfDocument, PdfConformance, CustomPdfConformance, ImageXObject, ColorBits, ColorSpace,
 };
 use std::fs;
 use std::fs::File;
@@ -14,8 +16,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let output_pdf_path = "./output.pdf"; // 输出的 PDF 文件名
 
     // 设定统一的 PDF 页面尺寸（A4）
-    let page_width = Mm(210.0); // A4宽度（毫米）
-    let page_height = Mm(297.0); // A4高度（毫米）
+    let mut page_width; // A4宽度（毫米）
+    let mut page_height; // A4高度（毫米）
+
+    let dpi = 200.0;
 
     // 读取文件夹内容
     let mut image_files: Vec<String> = fs::read_dir(folder_path)
@@ -41,15 +45,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("image_files len{}", image_files.len());
 
+    let img_path = Path::new(folder_path).join((image_files.get(0)).unwrap());
+    let img_dynamic = image::open(img_path)?; // 使用 image crate 打开图片
+    let (img_width, img_height) = img_dynamic.dimensions(); // 获取宽度和高度
+    let img_width_mm = px_to_mm(img_width as u64, dpi as u64);
+    let img_height_mm = px_to_mm(img_height as u64, dpi as u64);
+    page_width = img_width_mm;
+    page_height = img_height_mm;
     // 创建 PDF 文档
     let (mut document, page1, layer1) =
-        PdfDocument::new("My Document", page_width, page_height, "Layer 1");
+        PdfDocument::new("My Document", Mm(img_width_mm as f32), Mm(img_height_mm as f32), "Layer 1");
 
     document = document.with_conformance(PdfConformance::Custom(CustomPdfConformance {
         requires_icc_profile: false,
         requires_xmp_metadata: false,
         ..Default::default()
     }));
+
 
     let mut current_layer;
     let mut count = 0;
@@ -63,6 +75,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut buffer = [0; 8];
         file.read_exact(&mut buffer)?;
 
+        let img_path = Path::new(folder_path).join(&file_name);
+
+        let mut image_file = File::open(img_path).expect("无法打开图片文件");
+        let mut image_data = Vec::new();
+        image_file.read_to_end(&mut image_data).expect("读取图片文件失败");
+
+        let img_path = Path::new(folder_path).join(&file_name);
+        let img_dynamic = image::open(img_path)?; // 使用 image crate 打开图片
+        let (img_width, img_height) = img_dynamic.dimensions(); // 获取宽度和高度
+
+        println!("img_width is {}, img_height is {}", img_width, img_height);
+
+        let image_file_2 = ImageXObject {
+            width: Px(img_width as usize), // 根据实际图片的宽度设置
+            height: Px(img_height as usize), // 根据实际图片的高度设置
+            color_space: ColorSpace::Rgb, // 根据图片的颜色空间设置
+            bits_per_component: ColorBits::Bit16, // 根据图片的位深设置
+            interpolate: false,
+            image_data: image_data,
+            smask: None,
+            image_filter: None, // 目前不支持
+            clipping_bbox: None, // 目前不支持
+        };
+
+        // 将 ImageXObject 转换为 Image
+        let image2 = Image::from(image_file_2);
+
+
+
         // 根据读取的字节判断格式
         let format = match image::guess_format(&buffer) {
             Ok(format) => format,
@@ -73,90 +114,77 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         };
 
         current_layer = if count > 0 {
-            let (page, layer) = document.add_page(Mm(page_width.0), Mm(page_height.0), "Layer 1");
+            let img_path = Path::new(folder_path).join(&file_name);
+            let img_dynamic = image::open(img_path)?; // 使用 image crate 打开图片
+            let (img_width, img_height) = img_dynamic.dimensions(); // 获取宽度和高度
+            let img_width_mm = px_to_mm(img_width as u64, dpi as u64);
+            let img_height_mm = px_to_mm(img_height as u64, dpi as u64);
+            page_width = img_width_mm;
+            page_height = img_height_mm;
+            let (page, layer) = document.add_page(Mm(img_width_mm as f32), Mm(img_height_mm as f32), "Layer 1");
             document.get_page(page).get_layer(layer)
         } else {
             document.get_page(page1).get_layer(layer1)
         };
 
+        image2.add_to_layer(current_layer, ImageTransform::default());
+
         count += 1;
-        let img_path = Path::new(folder_path).join(&file_name);
-        let img = match format {
-            image::ImageFormat::Png => {
-                let mut image_file = File::open(img_path).unwrap();
-                let decoder = PngDecoder::new(&mut image_file)?;
-                let temp_image = Image::try_from(decoder).unwrap();
-                temp_image
-            }
-            image::ImageFormat::Jpeg => {
-                let mut image_file = File::open(img_path).unwrap();
-                let decoder = JpegDecoder::new(&mut image_file)?;
-                let temp_image = Image::try_from(decoder).unwrap();
-                temp_image
-            }
-            _ => {
-                eprintln!("不支持的图像格式");
-                return Err("不支持的图像格式".into());
-            }
-        };
 
-        img.add_to_layer(current_layer, ImageTransform::default());
-
-        // let img = image::open(img_path).expect("Failed to open image");
-
-        // // 将 DynamicImage 转换为适合 printpdf 的格式
-        // let (img_width, img_height) = img.dimensions();
-        // println!("img_width img_height {} {}", img_width, img_height);
-        // let img_bytes = img.to_rgba8().into_raw(); // 转换为 RGBA 格式的字节
-
-        // // 创建 ImageXObject
-        // let image_xobject = ImageXObject {
-        //     width: Px(img_width as usize),
-        //     height: Px(img_height as usize),
-        //     color_space: ColorSpace::Rgb, // 根据需要选择颜色空间
-        //     bits_per_component: ColorBits::Bit16,
-        //     interpolate: true,
-        //     image_data: img_bytes,
-        //     image_filter: None,
-        //     clipping_bbox: None,
-        //     smask: None,
+        // let img_path = Path::new(folder_path).join(&file_name);
+        // let img = match format {
+        //     image::ImageFormat::Png => {
+        //         let mut image_file = File::open(img_path).unwrap();
+        //         let decoder = PngDecoder::new(&mut image_file)?;
+        //         let temp_image = Image::try_from(decoder).unwrap();
+        //         temp_image
+        //     }
+        //     image::ImageFormat::Jpeg => {
+        //         let mut image_file = File::open(img_path).unwrap();
+        //         let decoder = JpegDecoder::new(&mut image_file)?;
+        //         let temp_image = Image::try_from(decoder).unwrap();
+        //         temp_image
+        //     }
+        //     image::ImageFormat::WebP => {
+        //         let mut image_file = File::open(img_path).unwrap();
+        //         let decoder = WebPDecoder::new(&mut image_file)?;
+        //         let temp_image = Image::try_from(decoder).unwrap();
+        //         temp_image
+        //     }
+        //     _ => {
+        //         eprintln!("不支持的图像格式");
+        //         return Err("不支持的图像格式".into());
+        //     }
         // };
 
-        // // 创建 PdfImage
-        // let pdf_image = PdfImage::from(image_xobject);
+        // let img_path = Path::new(folder_path).join(&file_name);
+        // let img_dynamic = image::open(img_path)?; // 使用 image crate 打开图片
+        // let (img_width, img_height) = img_dynamic.dimensions(); // 获取宽度和高度
+        // let img_width_mm = px_to_mm(img_width as u64, dpi as u64);
+        // let img_height_mm = px_to_mm(img_height as u64, dpi as u64);
 
         // // 计算缩放比例
-        // let img_aspect_ratio = img_width as f32 / img_height as f32;
-        // let page_aspect_ratio = page_width.0 / page_height.0;
+        // let scale_x = page_width / (img_width_mm);
+        // let scale_y = page_height / (img_height_mm);
+        // let scale = scale_x.min(scale_y); // 选择较小的比例以保持纵横比
+        // println!("scale is {} scale_x is {}, scale_y is {}", scale, scale_x, scale_y);
+        // // 计算居中位置
+        // // let translate_x = (page_width.0 - img_width as f32 * scale) / 2.0;
+        // // let translate_y = (page_height.0 - img_height as f32 * scale) / 2.0;
 
-        // let (final_width, final_height) = if img_aspect_ratio > page_aspect_ratio {
-        //     // 图片更宽，按宽度缩放
-        //     (page_width.0, page_width.0 / img_aspect_ratio)
-        // } else {
-        //     // 图片更高，按高度缩放
-        //     (page_height.0 * img_aspect_ratio, page_height.0)
+        // // 创建变换
+        // let transform = ImageTransform {
+        //     translate_x: None,
+        //     translate_y: None,
+        //     scale_x: Some(scale_x as f32),
+        //     scale_y: Some(scale_y as f32),
+        //     dpi: Some(300.0),
+        //     ..Default::default()
         // };
 
-        // // 添加新页面
-        // let (page, layer) = document.add_page(Mm(page_width.0), Mm(page_height.0), "Layer 1");
 
-        // // 获取当前页面的图层引用
-        // current_layer = document.get_page(page).get_layer(layer);
-
-        // // 计算居中位置
-        // let x_offset = (page_width.0 - final_width) / 2.0;
-        // let y_offset = (page_height.0 - final_height) / 2.0;
-
-        // // 将图片添加到 PDF 页面
-        // // pdf_image.add_to_layer(current_layer.clone(), ImageTransform {
-        // //     translate_x: Some(Mm(x_offset)),
-        // //     translate_y: Some(Mm(y_offset)),
-        // //     scale_x: None,
-        // //     scale_y: None,
-        // //     rotate: None,
-        // //     dpi: None,
-        // // });
-        // pdf_image.add_to_layer(current_layer, ImageTransform::default());
+        // // img.add_to_layer(current_layer, ImageTransform::default());
+        // img.add_to_layer(current_layer, transform);
     }
     println!("count is {}", count);
     // 保存 PDF 文件
@@ -166,27 +194,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-
-// // Compile with --feature="embedded_images"
-
-// // imports the `image` library with the exact version that we are using
-// use image::codecs::jpeg::JpegDecoder;
-// use printpdf::{ImageTransform, Image, Mm, PdfDocument};
-// use std::fs::File;
-// use std::io::BufWriter;
-
-// fn main() {
-//     let (doc, page1, layer1) = PdfDocument::new("PDF_Document_title", Mm(247.0), Mm(210.0), "Layer 1");
-//     let current_layer = doc.get_page(page1).get_layer(layer1);
-
-//     // currently, the only reliable file formats are bmp/jpeg/png
-//     // this is an issue of the image library, not a fault of printpdf
-//     let mut image_file = File::open("D:/115/115Chrome/mass/mass/zhong_mo_de_hou_gong_qi_xiang_qu/test/0.jpg").unwrap();
-//     let image = Image::try_from(JpegDecoder::new(&mut image_file).unwrap()).unwrap();
-
-//     // translate x, translate y, rotate, scale x, scale y
-//     // by default, an image is optimized to 300 DPI (if scale is None)
-//     // rotations and translations are always in relation to the lower left corner
-//     image.add_to_layer(current_layer.clone(), ImageTransform::default());
-//     doc.save(&mut BufWriter::new(File::create("./test.pdf").unwrap())).unwrap();
-// }
+fn px_to_mm(px: u64, dpi: u64) -> u64 {
+    (px / dpi) * 25.4 as u64
+}
